e4c97dd9eb6dd1cc72869276604221f5dfba98f1
razGit <razvaknin4@walla.com>
2016-06-03 13:00:01 +0300

    רז

--- LiadnRaz/DataBase.cpp
+++ LiadnRaz/DataBase.cpp
@@ -3,7 +3,7 @@ unordered_map<string, vector<string>> results;
 pair<string, vector<string>> p;
 DataBase::DataBase() throw(string)
 {
-	rc = 0;
+	int rc;
 	rc = sqlite3_open("C:\\Users\\User\\Documents\\GitHub\\LiadnRaz\\LiadnRaz.db", &db);
 	if (rc)
 	{
@@ -40,22 +40,46 @@ int callback(void* notUsed, int argc, char** argv, char** azCol)
 }
 bool DataBase::isUserExist(string username,char** azCol)
 {
-	rc = 0;
-	rc = sqlite3_exec(db, helper("select * from accounts where username=", username), callback, 0, &zErrMsg);
+	int rc;
+	char *save=new char[99];
+	strcpy(save, "select * from accounts where username=");
+	strcat(save, username.c_str());
+	rc = sqlite3_exec(db,save,callback,0,&zErrMsg);
 	if (sizeof(azCol)!= 0)
 	{
 		return true;
 	}
 	else
 	{
-		sqlite3_free(zErrMsg);
 		return false;
 	}
+}
+bool DataBase::isUserAndPassMatch(string username, string password, char **azCol)
+{
+	rc = 0;
+	char *sql = helper("select password from accounts where username =", username);
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	if (*azCol == password.c_str())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+char * DataBase::helper(char* command, string str)
+{
+	char *save = new char[99];
+	strcpy(save, command);
+	strcat(save, str.c_str());
+	return save;
 }
 bool DataBase::addNewUser(string username, string password, string email)
 {
 	rc = 0;
-	char *sql=helper(helper(helper(helper("insert into accounts(username,password,email)values(", username),password),email),")");
+	char *sql = helper(helper(helper(helper("insert into accounts(username,password,email)values(", username), password), email), ")");
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 	if (rc != SQLITE_OK)
 	{
@@ -66,26 +90,4 @@ bool DataBase::addNewUser(string username, string password, string email)
 	{
 		return true;
 	}
-}
-bool DataBase::isUserAndPassMatch(string username, string password,char **azCol)
-{
-	rc = 0;
-	char *sql = helper("select password from accounts where username =", username);
-	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-	if(*azCol == password.c_str())
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-	
-}
-char * DataBase::helper(char* command,string str)
-{
-	char *save = new char[99];
-	strcpy(save, command);
-	strcat(save, str.c_str());
-	return save;
 }
